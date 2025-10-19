cb5cd8eeaec821214e959057094f2fbe37ae2a40
simon <simonwho22@gmail.com>
2025-10-19 19:03:30 -0400

    created Upgrade class so creating future upgrades in the game is easier

---
+++ src/Upgrade.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.Scanner;
+
+public class Upgrade extends JLabel {
+    private JButton buyButton = new JButton("BUY");
+    private JTextField upgradeName = new JTextField();
+    private JTextField upgradePrice = new JTextField();
+    private int scalingID;
+    public Upgrade(Gameplay gameplay, int upgradeID) {
+
+
+        //scalingID = priceScalingID;
+        //upgradeName.setText(name);
+        //upgradePrice.setText(
+    }
+}
