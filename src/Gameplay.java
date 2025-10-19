005307186dfc15b9ce9a22675363440b31df082d
simon <simonwho22@gmail.com>
2025-10-19 19:04:23 -0400

    implemented basic file reader to keep track of upgrades

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -4,10 +4,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gameplay extends JLayeredPane implements ActionListener{
     //0 = coins
+    private String upgradeIDPath = "upgradeData.csv";
     private int[] statArray = new int[5];
+    private List<Item> itemList = new ArrayList<Item>();
     private double ticks = 0.0;
     private Timer timer;
     long lastTime = System.nanoTime();
@@ -29,6 +37,7 @@ public class Gameplay extends JLayeredPane implements ActionListener{
             }
         });
         initializeStats();
+        loadItems();
     }
 
     public void paintComponent(Graphics g)
@@ -69,12 +78,28 @@ public class Gameplay extends JLayeredPane implements ActionListener{
     {
         statDisplay.setText(statArray[0]+"");
         statDisplay.setFont(new Font("Times New Roman", Font.BOLD, 20));
-        statDisplay.setForeground(Color.RED);
+        statDisplay.setForeground(Color.GREEN);
         statDisplay.setHorizontalAlignment(JLabel.CENTER);
-        statDisplay.setBounds(900,100,100,100);
+        statDisplay.setBounds(900,0,100,30);
         statDisplay.setEditable(false);
         statDisplay.setVisible(true);
-        statDisplay.setBackground(Color.LIGHT_GRAY);
+        statDisplay.setBackground(Color.BLACK);
         add(statDisplay);
+    }
+
+    public void loadItems() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(upgradeIDPath));
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                String[] item = line.split(",");
+                itemList.add(new Item(item[0],item[1],Integer.parseInt(item[2]),Integer.parseInt(item[3]),Integer.parseInt(item[4])));
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            System.out.println("Error opening upgrade ID file");
+        }
     }
 }
