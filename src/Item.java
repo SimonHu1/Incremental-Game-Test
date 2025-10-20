f88bbc70fc5e352ca0caa01f9a1fada5fcbe1f33
simon <simonwho22@gmail.com>
2025-10-20 00:03:18 -0400

    added getter methods for all attributes of Item class

--- src/Item.java
+++ src/Item.java
@@ -1,14 +1,42 @@
 public class Item {
     private String itemName;
     private String itemDescription;
+    private int itemCurrency;
     private int itemID;
     private int itemBasePrice;
     private int itemScalingID;
-    public Item(String itemName, String itemDescription, int itemID, int itemBasePrice, int itemScalingID) {
+    private int purchasedAmount;
+    public Item(String itemName, String itemDescription, int itemCurrency, int itemID, int itemBasePrice, int purchasedAmount, int itemScalingID) {
         this.itemName = itemName;
         this.itemDescription = itemDescription;
+        this.itemCurrency = itemCurrency;
         this.itemID = itemID;
         this.itemBasePrice = itemBasePrice;
+        this.purchasedAmount = purchasedAmount;
         this.itemScalingID = itemScalingID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public int getItemCurrency() {
+        return itemCurrency;
+    }
+
+    public int getItemBasePrice() {
+        return itemBasePrice;
+    }
+
+    public int getItemScalingID() {
+        return itemScalingID;
+    }
+
+    public int getPurchasedAmount() {
+        return purchasedAmount;
     }
 }
